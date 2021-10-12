cc5da153a375ca99ffae9880700ad1598e90bd70
Sven Jäger <sv_jaeger@gmx.de>
2021-10-12 22:04:55 +0200

    blubb

--- platform.py
+++ platform.py
@@ -6,19 +6,14 @@
 from platform import system
 from platformio.managers.platform import PlatformBase
 
+
 class Mt2502Platform(PlatformBase):
     def configure_default_packages(self, variables, target):
-        
+
         # configure script based on MCU type
         board_config = self.board_config(variables.get("board"))
         mcu = variables.get("board_build.mcu",
                             board_config.get("build.mcu", "MT2502"))
-        
-        frameworks = variables.get("pioframework", [])
-        if "arduino" in frameworks:
-            self.frameworks["arduino"]["package"] = "framework-mt2502arduino"
-            self.packages["framework-mt2502arduino"]["optional"] = False
-            self.packages["framework-mt2502arduino"]["optional"] = True
 
         return PlatformBase.configure_default_packages(self, variables,
                                                        target)
